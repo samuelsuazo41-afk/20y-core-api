dc0b87d74f52a90182021b68b590cfae88fbcf98
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-05-05 10:28:15 +0200

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,3 @@
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request))); 
